68630dfce93c64b9d2d25945309972587177045e
PrzemoNo1 <PrzemoNo1@PrzemoNo1.example.com>
2020-01-05 23:20:52 +0100

    Add background

--- src/weather_app.py
+++ src/weather_app.py
@@ -7,6 +7,10 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+background_image = tk.PhotoImage(file='./src/landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 frame = tk.Frame(root, bg='blue', bd=5)
 frame.place(relx=0.5, rely = 0.2, relwidth=0.7, relheight=0.1, anchor='n')
 
